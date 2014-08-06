d102f2a7ace94ca2846f03788a7f907c719650d1
Terry Ebaugh <tebaugh@gmail.com>
2014-08-06 16:48:49 -0400

    Trying pip install -r as a test

--- jenkins/build_ex_sh.sh
+++ jenkins/build_ex_sh.sh
@@ -20,8 +20,9 @@ fi
 # Create virtualenv and install necessary packages
 virtualenv --no-site-packages $PYENV_HOME
 . $PYENV_HOME/bin/activate
-pip install --quiet nosexcover
-pip install --quiet pylint
+#pip install --quiet nosexcover
+#pip install --quiet pylint
 pip install --quiet $WORKSPACE/  # where your setup.py lives
+pip install -r requirements.txt
 #nosetests --with-xcoverage --with-xunit --cover-package=myapp --cover-erase
 #pylint -f parseable myapp/ | tee pylint.out
