1a537ffa74c70d6a724f9805e383f57d8d6399b0
Terry Ebaugh <tebaugh@gmail.com>
2014-08-06 15:51:19 -0400

    Testing removing modules

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python
 from distutils.core import setup
 setup(name='p_test',
-      version='1.0',
-      py_modules=['p_test'],
+      version='0.1.0',
+      description="p_test us just used to test building and packaging python with jenkins"
+      # py_modules=['p_test'],
       )
