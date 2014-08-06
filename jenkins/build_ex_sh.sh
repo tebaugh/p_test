3d66d945f7cbf8d4545f40e7b67ebf7b58323de0
Terry Ebaugh <tebaugh@gmail.com>
2014-08-06 16:53:46 -0400

    qualified path for requirements file

--- jenkins/build_ex_sh.sh
+++ jenkins/build_ex_sh.sh
@@ -23,6 +23,6 @@ virtualenv --no-site-packages $PYENV_HOME
 #pip install --quiet nosexcover
 #pip install --quiet pylint
 pip install --quiet $WORKSPACE/  # where your setup.py lives
-pip install -r requirements.txt
+pip install -r $PYENV_HOME/requirements.txt
 #nosetests --with-xcoverage --with-xunit --cover-package=myapp --cover-erase
 #pylint -f parseable myapp/ | tee pylint.out
